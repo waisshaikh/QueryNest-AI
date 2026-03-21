4d500865bb7cb6614d2da6e420fb9ffd74a7fa6f
waisshaikh <waisshaikh394@gmail.com>
2026-03-21 21:39:44 +0530

    add user, chat, and message models; integrate bcrypt for password hashing

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ import app from "./src/app.js"
 
 ConnectToDb();
 
+app.get("/",(req,res)=>{
+    res.send("server is running on port 3000")
+})
+
 app.listen(3000,(req,res)=>{
     console.log("Server is running on port 3000");
     
