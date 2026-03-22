cd6357ace69bebade4bc31e6341cd79700818937
waisshaikh <waisshaikh394@gmail.com>
2026-03-22 21:02:52 +0530

    fix: correct user registration logic and improve error handling for existing users

--- backend/src/Controller/auth.controller.js
+++ backend/src/Controller/auth.controller.js
@@ -3,8 +3,20 @@ import userModel from "../models/user.model";
 import jwt from "jsonwebtoken";
 
 export async function register(req,res) {
-    const {username,email,password}=req.body;
+    const {username,password,email}=req.body;
+    
+    const isUserExist = await userModel.findOne({
+        $or:[{username},{email}]
+    })
+    if(isUserExist){
+        res.status(402).json({
+            "message": "User Alreday Exist with this email",
+            success:false,
+            
+        })
+    }
+
+    const User = userModel.create({username ,email ,password})
     
     
 }
-
