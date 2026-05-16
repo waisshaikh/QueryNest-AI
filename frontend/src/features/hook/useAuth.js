801c461a0e8296e79916d297264bdc21a07d0acf
waisshaikh <waisshaikh394@gmail.com>
2026-05-16 17:05:06 +0530

    feat: implement authentication functions for registration, login, and fetching user data

--- frontend/src/features/hook/useAuth.js
+++ frontend/src/features/hook/useAuth.js
@@ -6,4 +6,54 @@ import  {setUser,setError,setLoading,} from "../auth/auth.slice"
 export function auth (){
     const Dispatch = useDispatch()
     
+    async function handleRegister({email, username, password}) {
+        try{
+            Dispatch(setLoading(true))
+            const data = await register ({email, username, password})
+
+        }catch(error) {
+            Dispatch(setError(error.response?.data?.message|| "registration failed"))
+        }finally{
+            Dispatch(setLoading(false))
+        }
+        
+        
+    }
+
+
+    async function handleLogin({email, password}) {
+
+        try{
+            Dispatch(setLoading(true))
+            const data = await login({email,password})
+            Dispatch(setUser(data.user))
+        }catch(error){
+            Dispatch(setError(error.response?.data?.message||"login failed"))
+        }finally{
+            Dispatch(setLoading(false))
+        }  
+    }
+
+    async function handleGetMe () {
+        try{
+            Dispatch(setLoading(true))
+            const data = await getme()
+            Dispatch(setUser)(data.user)
+        }catch(err){
+            Dispatch(setError(err.response?.data?.message||"failed to fetch user data "))
+
+        }finally{
+            Dispatch(setLoading(false))
+
+        }
+        
+    } 
+    
+
+    return{
+        handleRegister,
+        handleLogin,
+        handleGetMe
+    }
+    
 }
