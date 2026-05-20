57bd8be0297263ce2306507a326599b7ab619f15
Wais Shaikh <waisshaikh394@gmail.com>
2026-05-20 22:22:31 +0530

    fix: correct loading and user authentication handling in Protected component

--- frontend/src/features/auth/components/protected.jsx
+++ frontend/src/features/auth/components/protected.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import {Navigate} from 'react-router'
 
 const protected = ({children}) => {
     const user = useSelector(state=> state.auth.user)
     const loading = useSelector(state=> state.auth.loading)
 
+if (loading) {
+  return <div>Loading......</div>
+}
 
+if (!user){
+  return <Navigate to = "/login" replce/>
+}
    
 
   return (
