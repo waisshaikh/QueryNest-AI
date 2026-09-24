a3859137b9526f7cf6869fce1417ceb1754d75dd
Wais Shaikh <waisshaikh394@gmail.com>
2026-09-25 01:30:37 +0530

    feat: add mail service using nodemailer for sending emails

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -2,14 +2,13 @@ import dotevn from "dotenv";
 import { config } from "dotenv";
 import nodeMailer from "nodemailer"
 
+config(); // load .env variables
+
 const transporter = nodeMailer.createTransport({
   service: 'gmail',
-  auth:{
-    type:'OAuth2',
-    user:process.env.GOOGLE_USER,
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken:process.env.GOOGLE_REFRESH_TOKEN
+  auth: {
+    user: process.env.GOOGLE_USER,
+    pass: process.env.GOOGLE_APP_PASSWORD,
   }
 })
 
@@ -23,14 +22,16 @@ transporter.verify((error) => {
 
 
 //  send email function
-const sendEmail = async (to, subject, text, html) => {
+
+const sendEmail = async ({ to, subject, text, html }) => {
+  console.log("📧 Sending email to:", to);
   try {
     const info = await transporter.sendMail({
       from: `QuerNest <${process.env.GOOGLE_USER}>`,
-      to, 
-      subject, 
-      text, 
-      html, 
+      to,
+      subject,
+      text,
+      html,
     });
 
     console.log('Message sent: %s', info.messageId);
