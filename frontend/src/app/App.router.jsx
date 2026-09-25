f18e9244e1e44bf57c5728207257c7c8c17be2b9
Wais Shaikh <waisshaikh394@gmail.com>
2026-09-25 18:13:42 +0530

    feat: implement RootLayout for handling user authentication and loading state in routing

--- frontend/src/app/App.router.jsx
+++ frontend/src/app/App.router.jsx
@@ -1,25 +1,39 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
 import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
-import { Navigate } from "react-router";
 import Dashboard from "../features/services/chat/pages/Dashboard";
 import Protected  from "../features/auth/components/protected.jsx"
+import { useAuth } from "../features/hook/useAuth";
+import { useEffect } from "react";
+
+function RootLayout() {
+    const { handleGetMe } = useAuth();
+
+    useEffect(() => {
+        handleGetMe();
+    }, []);
+
+    return <Outlet />;
+}
 
 export const router = createBrowserRouter([
     {
-        path: "/login",
-        element: <Login />
-    },
-    {
-        path: "/register",
-        element: <Register />
-    },
-    {
-        path: "/",
-
-        element:<Protected>
-        <Dashboard/>
-        </Protected>
-    },
-    
+        element: <RootLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "/register",
+                element: <Register />
+            },
+            {
+                path: "/",
+                element: <Protected>
+                    <Dashboard />
+                </Protected>
+            },
+        ]
+    }
 ])
--- frontend/src/features/auth/components/protected.jsx
+++ frontend/src/features/auth/components/protected.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import {Navigate} from 'react-router'
+import {Navigate, RouterProvider} from 'react-router'
 
 const ProtectedRoute = ({children}) => {
     const user = useSelector(state=> state.auth.user)
     const loading = useSelector(state=> state.auth.loading)
 
-// Redirect to login immediately if loading is done and no user
-if (!loading && !user) {
-  return <Navigate to="/login" replace />
+if(loading){
+  return <div>Loading.......</div>
 }
 
-// Show loading while checking authentication
-if (loading) {
-  return <div>Loading......</div>
+if(!user){
+  return <Navigate to = "/login" replace/>
 }
 
 return children
