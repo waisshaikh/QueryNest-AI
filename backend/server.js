a89f8e70743600953515640dfe0aae4ac488f29b
Wais Shaikh <waisshaikh394@gmail.com>
2026-09-25 19:50:17 +0530

    feat: integrate socket.io for real-time communication
    - Added socket.io server initialization in server.js
    - Created a new socket server file to handle socket connections
    - Updated package.json and package-lock.json to include socket.io and socket.io-client
    - Implemented basic socket connection handling in the new server.socket.js file
    - Created a Dashboard component to utilize Redux state for user information

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,46 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+import http from "http"
 import ConnectToDb from "./src/config/DatabseConnetion.js";
-import { testAi } from "./src/services/ai.service.js";
-
-
+// import { testAi } from "./src/services/ai.service.js";
 import app from "./src/app.js";
+import { initSocket } from "./src/sockets/server.socket.js";
 
-ConnectToDb();
 
-if (process.env.RUN_AI_STARTUP_TEST === "true") {
-    testAi().catch((error) => {
-        console.error("AI startup test failed:", error.message);
-    });
-}
+
+const PORT = process.env.PORT || 3000;
+
+const httpServer = http.createServer(app)
+initSocket(httpServer)
+
+
+ConnectToDb()
+    .catch((err)=>{
+        console.error("mongo connection failed",err)
+        process.exit(1)
+    })
+
+
+
+// if (process.env.RUN_AI_STARTUP_TEST === "true") {
+//     testAi().catch((error) => {
+//         console.error("AI startup test failed:", error.message);
+//     });
+// }
+
+
 
 app.get("/",(req,res)=>{
     res.send(`server is running on port ${process.env.PORT || 3000}`)
 })
 
-testAi()
 
-const PORT = process.env.PORT || 3000;
+// testAi()
 
-app.listen(PORT,()=>{
+
+
+
+httpServer.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     
 })
---
+++ backend/src/sockets/server.socket.js
@@ -0,0 +1,27 @@
+import { Server } from "socket.io"
+
+let io;
+
+export function initSocket(httpServer) {
+    io = new Server(httpServer, {
+        cors: {
+            origin: "http://localhost:5173",
+            Credential: true
+
+        }
+    })
+
+    console.log("socket io server is runnig")
+
+    io.on("connection", (socket) => {
+        console.log("A User Connected" + socket.id)
+    })
+}
+
+export function getIo() {
+    if (!io) {
+        throw new Error("Socket.io is not initialized")
+    }
+
+    return io
+}
