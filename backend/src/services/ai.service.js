98d0a8e40b2d67d6c6a8756b6dbd114c5bc21d99
Wais Shaikh <waisshaikh394@gmail.com>
2026-09-25 11:35:28 +0530

    fix: update AI model version and modify test query in ai.service.js

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -5,12 +5,12 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 
 const model = new ChatGoogleGenerativeAI({
-  model: "gemini-3.7-flash",
+  model: "gemini-3.6-flash",
   apiKey: process.env.GEMINI_API_KEY
 });
 
 export async function testAi() {
-  model.invoke("what is capital of India?").then((response)=>{
+  model.invoke("what is thermo dynamics explain under 100 words?").then((response)=>{
     console.log(response.text)
   })
 
